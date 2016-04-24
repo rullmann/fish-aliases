a3864572f5e1e481e4c0de19ffef243b1a236e1e
Rico Ullmann <rico@erinnerungsfragmente.de>
2016-04-24 14:33:00 +0200

    add psgrep to find processes easily

--- init.fish
+++ init.fish
@@ -25,3 +25,20 @@ function ds --description 'calculate size of current or a given dir and sort by 
 
 	end
 end
+
+function psgrep --description 'show matching processes for $argv'
+	switch (count $argv)
+
+	case 0
+		echo -e "No argument given!\nDo you expect me to guess what you're looking for?"
+		return 1
+
+	case 1
+		ps aux | grep $argv | grep -v grep
+
+	case \*
+		echo "Only 1 arguemnt is valid!"
+		return 1
+
+	end
+end
