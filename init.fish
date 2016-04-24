2f614ca56103c4d94b4180eb56e02defad22284b
Rico Ullmann <rico@erinnerungsfragmente.de>
2016-04-24 14:40:07 +0200

    add nsgrep to find open ports easily

--- init.fish
+++ init.fish
@@ -42,3 +42,20 @@ function psgrep --description 'show matching processes for $argv'
 
 	end
 end
+
+function nsgrep --description 'show open ports for $argv'
+	switch (count $argv)
+
+	case 0
+		echo -e "No argument given!\nDo you expect me to guess what you're looking for?"
+		return 1
+
+	case 1
+		netstat -tulpen | grep $1 | grep -v grep
+
+	case \*
+		echo "Only 1 arguemnt is valid!"
+		return 1
+
+	end
+end
